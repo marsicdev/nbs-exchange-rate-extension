2cf87dd30be1eba0e0e6133a8b165523ae71b76e
Marko Arsić <marko@hypetech.io>
2023-06-02 15:04:33 +0200

    Add readme and prepare first version

---
+++ README.md
@@ -0,0 +1,49 @@
+# NBS Exchange Rate - Chrome Extension
+
+The **NBS Exchange Rate Chrome Extension** is a widget that allows users to quickly view **Official Middle RSD Exchange Rate** from the official National Bank of Serbia [website](https://www.nbs.rs).
+
+> This is unofficial extension and is not affiliated with NBS.
+
+## Features
+
+-   Displays real-time exchange rates parsed from [official NBS rates page](https://www.nbs.rs/kursnaListaModul/srednjiKurs.faces?lang=eng).
+-   Retrieves exchange rate data directly from the National Bank of Serbia website.
+-   Updates exchange rates automatically upon opening the extension popup.
+-   Simple and intuitive user interface.
+-   Works offline once exchange rates are fetched.
+-   Light (default) or dark mode based on your system theme.
+
+#### TBD - Upcoming features and improvements
+
+-   [ ] Language switcher
+-   [ ] Theme switcher
+-   [ ] Favorites
+-   [ ] Refetch rates
+-   [ ] Migrate to TS and MVC
+
+## Usage
+
+1.  Click on the NBS Exchange Rate extension icon next to the address bar to open the popup window.
+2.  The popup window will display the loading indicator while fetching the latest exchange rates.
+3.  Once the exchange rates are fetched, the popup window will update with the current rates.
+4.  The exchange rates will be displayed under their respective currency labels.
+5.  The popup window will update the exchange rates automatically whenever you open it.
+
+## Development
+
+To install the NBS Exchange Rate Chrome Extension for development, follow these steps:
+
+1.  Clone or download this repository to your local machine.
+2.  Open Google Chrome and go to the Extensions page by typing `chrome://extensions` in the address bar.
+3.  Enable the "Developer mode" toggle switch at the top right corner of the Extensions page.
+4.  Click on the "Load unpacked" button on the top left corner of the Extensions page.
+5.  Select the folder where you cloned or downloaded the extension files.
+6.  The NBS Exchange Rate extension will be added to your Chrome browser.
+
+## Support
+
+If you encounter any issues or have any questions or suggestions, please feel free to [open an issue](https://github.com/marsicdev/nbs-exchange-rate-extension/issues) on the GitHub repository for this project.
+
+## License
+
+This Chrome extension is open source and available under the [MIT License](https://github.com/marsicdev/nbs-exchange-rate-extension/blob/main/LICENSE).
---
+++ index.js
@@ -0,0 +1,184 @@
+//@ts-check
+const NBS_EXCHANGE_RATE_URL =
+    'https://www.nbs.rs/kursnaListaModul/srednjiKurs.faces?lang=eng'
+
+const DOMSelector = Object.freeze({
+    htmlTag: 'html',
+    markTag: 'mark',
+    ratesTableRow: '.indexsrednjiKursListaTable tr',
+    ratesList: '.rates-list',
+    updateDateTimeText: '.update-date-time',
+    infoText: '.info',
+    logoImage: '.logo',
+})
+
+function fetchExchangeRates() {
+    const nbsCorsEndpoint = `https://cors.hypetech.xyz/${NBS_EXCHANGE_RATE_URL}`
+    fetch(nbsCorsEndpoint)
+        .then((response) => response.text())
+        .then((data) => {
+            displayLoading()
+
+            const parser = new DOMParser()
+            const htmlDoc = parser.parseFromString(data, 'text/html')
+
+            const currencyList = []
+
+            // Get currency list
+            htmlDoc.querySelectorAll(DOMSelector.ratesTableRow).forEach((elem, index) => {
+                if (!index) return
+                currencyList.push(elem)
+            })
+
+            if (currencyList.length === 0) {
+                displayError("Couldn't fetch exchange rates.")
+                return
+            }
+
+            // Create HTML output
+            let htmlOutput = '<div>'
+            currencyList.forEach((elem) => {
+                const exchangeRateRow = elem.innerHTML
+                    .trim()
+                    .replaceAll(` tabindex="0"`, '')
+                    .replaceAll(`<td>`, '')
+                    .split('</td>')
+                    .map((item) => item.trim())
+
+                htmlOutput += createCardHTML(exchangeRateRow)
+            })
+
+            // End HTML output
+            htmlOutput += '</div>'
+
+            // Display HTML output
+            const $ratesList = document.querySelector(DOMSelector.ratesList)
+
+            if (!$ratesList) return //TODO: Handle error
+
+            $ratesList.innerHTML = htmlOutput
+
+            // Setup listeners for copy to clipboard
+            document.querySelectorAll(DOMSelector.markTag).forEach(($mark) => {
+                $mark?.addEventListener('click', attachCopyListener.bind(null, $mark))
+            })
+
+            // Update update date time text content with fetch date time
+            const $updateDateTime = document.querySelector(DOMSelector.updateDateTimeText)
+            if ($updateDateTime) {
+                $updateDateTime.textContent = formatUpdateDateTime()
+            }
+        })
+        .catch((error) => {
+            displayError()
+            console.error('Error fetching exchange rates:', error)
+        })
+        .finally(() => {
+            displayLoading(false)
+        })
+}
+
+function formatUpdateDateTime() {
+    const options = {
+        year: 'numeric',
+        month: 'numeric',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric',
+        hour12: false,
+    }
+
+    // @ts-ignore
+    return new Intl.DateTimeFormat(undefined, options).format(new Date())
+}
+
+function attachCopyListener($mark) {
+    const clipboardText = $mark.textContent
+
+    if (!clipboardText) return
+
+    navigator.clipboard.writeText(clipboardText)
+    const $info = document.querySelector(DOMSelector.infoText)
+
+    if (!$info) return
+
+    $info.classList.remove('hidden')
+    $info.textContent = 'Copied to clipboard'
+
+    setTimeout(() => {
+        $info?.classList.add('hidden')
+    }, 1500)
+}
+
+function createCardHTML(exchangeRateRow) {
+    const [label, code, country, unit, rate] = exchangeRateRow
+    return `
+        <article>
+            <hgroup>
+                <small>${country}</small>
+                <h2><span>${label}<span> <mark data-tooltip="Copy">${rate}</mark></h2>
+                <h3><small>${unit} ${label} = ${rate} RSD</small></h3>
+            </hgroup>
+        </article>
+    `
+}
+
+function displayLoading(loading = true) {
+    const $logoImage = document.querySelector('img')
+
+    if (loading) {
+        $logoImage?.classList.add('shimmer')
+    } else {
+        // Add delay to show shimmer effect on fast loading
+        setTimeout(() => {
+            $logoImage?.classList.remove('shimmer')
+        }, 1000)
+    }
+}
+
+function displayError(msg = 'Error fetching exchange rates') {
+    const $main = document.querySelector('main')
+
+    if (!$main) return
+
+    $main.innerHTML = `<div class="box"><kbd>${msg}<kbd></div>`
+}
+
+function detectTheme() {
+    const darkThemeMq = window.matchMedia('(prefers-color-scheme: dark)')
+
+    const theme = darkThemeMq.matches ? 'dark' : 'light'
+    document.querySelector(DOMSelector.htmlTag)?.setAttribute('data-theme', theme)
+}
+
+function setupListeners() {
+    document.querySelector('.nbs')?.addEventListener('click', () => {
+        // @ts-ignore
+        chrome.tabs.create({
+            url: NBS_EXCHANGE_RATE_URL,
+        })
+    })
+
+    document.querySelector('.about')?.addEventListener('click', () => {
+        alert(`This is unofficial extension and is not affiliated with NBS.\n
+Purpose of this extension is to provide a quick access and information about official middle RSD(Serbian Dinar) exchange rate.\n
+Published under MIT license.\n
+Developed by @marsicdev.`)
+    })
+
+    document.querySelector('.developer')?.addEventListener('click', () => {
+        // @ts-ignore
+        chrome.tabs.create({
+            url: 'https://github.com/marsicdev',
+        })
+    })
+}
+
+function init() {
+    detectTheme()
+    setupListeners()
+    fetchExchangeRates()
+}
+
+document.addEventListener('DOMContentLoaded', init)
